9e340dd9f3799f3a7d2e64a6441a332b55c2e708
Justin Chacon <justin@Justins-MacBook-Air-2.local>
2025-03-17 19:25:56 -0400

    Midterm update

--- ClientSite/avaSite/src/components/Navbar.jsx
+++ ClientSite/avaSite/src/components/Navbar.jsx
@@ -10,23 +10,23 @@ export default function Navbar() {
 
     return (
         <div className="navbar">
-        <button id="close">X</button>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink id="/home" className="NavLink" to="/home">Ava Moniz</NavLink>
-                </li>
-                <li>
-                    <NavLink id="/" className="NavLink" to="/">Information</NavLink>
-                </li>
-                <li>
-                    <NavLink id="/films" className="NavLink" to="/films">Films</NavLink>
-                </li>
-                <li>
-                    <NavLink id="/messages" className="NavLink" to="/messages">Messages</NavLink>
-                </li>
-            </ul>
-        </nav>
-    </div>
+            <button id="close" aria-label="Close navigation menu">X</button>
+            <nav>
+                <ul>
+                    <li>
+                        <h1>Ava Moniz</h1>
+                    </li>
+                    <li>
+                        <NavLink id="/" className="NavLink" to="/" aria-label="Navigate to Information page">Information</NavLink>
+                    </li>
+                    <li>
+                        <NavLink id="/films" className="NavLink" to="/films" aria-label="Navigate to Films page">Films</NavLink>
+                    </li>
+                    <li>
+                        <NavLink id="/messages" className="NavLink" to="/messages" aria-label="Navigate to Messages page">Messages</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
--- ClientSite/avaSite/src/pages/InformationPage.jsx
+++ ClientSite/avaSite/src/pages/InformationPage.jsx
@@ -5,6 +5,14 @@ import $ from 'jquery'
 
 function InformationPage() {
     const [info, setInfo] = useState(<div></div>);
+    const [spotlight, setSpotlight] = useState("");
+
+    // using art institute of chicago api to fetch avas current favorite work
+    useEffect(() => {
+        fetch("https://www.artic.edu/iiif/2/2d484387-2509-5e8e-2c43-22f9981972eb/full/843,/0/default.jpg")
+            .then(response => response.url)
+            .then(url => setSpotlight(url));
+    }, []);
     // fetches the information from the info.html file
     useEffect(() => {
           async function fetchInfo() {
@@ -43,6 +51,11 @@ return (
             </div>
         </div>
         <div className="summary" dangerouslySetInnerHTML={{__html: info}}>
+        </div>
+        <h2>Famous Work Spotlight</h2>
+        <div className='spotlight'>
+        <img src={spotlight} id='spotlight' alt="Famous Work Spotlight"/>
+        <p>Here is Ava's current favorite work from her schools archive, titled 'A Sunday on La Grande Jatte' (1884)</p>
         </div>
     </div>
     </>
